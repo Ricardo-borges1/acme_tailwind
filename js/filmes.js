501dc20caaa53d7ac4ff47995c6e8551f6ae9eed
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2024-03-14 17:12:59 -0300

    primeira tela finalizada

--- js/filmes.js
+++ js/filmes.js
@@ -2,7 +2,6 @@ export async function getFilmes () {
    const url = 'http://localhost:8080/v2/filmesAcme/filmes' 
    const response = await fetch (url)
    const data = await response.json()
-
     // ou usar return data para mostrar todas as informações.
    return data.filmes
 }
@@ -14,4 +13,27 @@ export async function getFilme (id) {
     const data = await response.json ()
 
     return data.filme[0]
+}
+
+export async function postFilme(filme) {
+     const url = 'http://localhost:8080/v2/filmesAcme/filme'
+     const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(filme)
+     }
+     const response = await fetch (url,options)
+     return response.ok
+
+}
+
+export async function deleteFilme(id){
+    const url = `http://localhost:8080/v1/filmesAcme/deleteFilme/${id}`
+    const options = {
+        method: 'DELETE'
+    }
+    const response = await fetch ( url, options)
+    return response.ok
 }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 'use strict'
 
-import { getFilmes, getFilme} from "./filmes.js"
+import { getFilmes, getFilme, postFilme, deleteFilme} from "./filmes.js"
 
 
 function criarCard (filme){
 
     const card = document.createElement('div')
+    card.classList.add('flex', 'flex-col', 'bg-white', 'rounded-lg', 'shadow-lg', 'p-4', 'mb-2', 'transform', 'trnsition', 'hover:scale-105', 'duration-300', 'max-w-sm', 'mx-auto')
 
     const titulo = document.createElement('h2')
-    //titulo.textContent = filme.nome
+    titulo.classList.add('text-lg', 'font-bold', 'text-red-700', 'mb-2', 'text-center')
+    titulo.textContent = filme.nome
 
     //const texto = document.createElement('textarea')
     //texto.textContent = filme.sinopse
@@ -16,11 +18,23 @@ function criarCard (filme){
     const capa = document.createElement('img')
     capa.src = filme.foto_capa
 
-    card.append(titulo,capa)
+    const preco = document.createElement('p')
+    preco.classList.add('text-md', 'text-gray-800', 'font-bold', 'mb-2', 'text-center', 'pt-2')
+    preco.textContent = `Preço: R$${filme.valor_unitario}`
+
+
+    const duracao = document.createElement('p')
+    duracao.classList.add('text-md', 'text-red-800', 'font-bold', 'mb-2', 'text-center', 'pt-2')
+    duracao.textContent = filme.duracao
+    card.append(titulo,capa,preco,duracao)
 
     
-    return card
+    return card    
 }
+
+
+
+
 
 async function preencherContainer() {
     const container = document.querySelector('#lista')
@@ -35,4 +49,18 @@ async function preencherContainer() {
     })
 }
 
+// adicionar novo filme
+const filme = {
+
+    "nome": "Oppenheimer",
+            "sinopse": "O físico J. Robert Oppenheimer trabalha com uma equipe de cientistas durante o Projeto Manhattan, levando ao desenvolvimento da bomba atômica.",
+            "duracao": "03:09:09",
+            "data_lancamento": "2024-02-08",
+            "data_relancamento": "2021-03-09",
+            "foto_capa": "https://upload.wikimedia.org/wikipedia/pt/thumb/5/5b/Oppenheimer_poster.jpg/250px-Oppenheimer_poster.jpg",
+            "valor_unitario": 40
+}
+
+deleteFilme(17)
+//postFilme(filme)
 preencherContainer()
